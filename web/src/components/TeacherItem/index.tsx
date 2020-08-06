ec76785502be97ceed52b6cf79807878548bb8fe
Mário Santos <mariocsants@gmail.com>
2020-08-05 21:37:43 -0300

    Terceira aula - finalizando fron-end web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/13947440?s=460&u=26b5d9b410c2a03e76807f370e0e55d03438cec4&v=4" alt="Mário Santos" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Mário Santos</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            <br /> <br />
-            Ab voluptatum ratione velit tempora culpa iste minima tempore, ex quod natus quasi commodi officiis? Quibusdam tempora dolor necessitatibus cupiditate molestias sed?
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          href={`https://wa.me/${teacher.whatsapp}`}
+          onClick={createNewConnection}
+          target="_blank"
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
+            Entrar em contato
+          </a>
       </footer>
     </article>
   );
