cc04525f82d45b13ef0bf09f7c88be390a4615fd
Mário Santos <mariocsants@gmail.com>
2020-08-07 23:02:12 -0300

    Última aula - finalizando projeto

---
+++ mobile/src/routes/AppStack.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import StudyTabs from './StudyTabs';
+
+import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function AppSnack() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Landing" component={Landing}/>
+        <Screen name="GiveClasses" component={GiveClasses}/>
+        <Screen name="Study" component={StudyTabs}/>
+      </Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default AppSnack;
